79312eb917d1fa622e13b66b9004923daae9f360
siart24 <bleart.lema@stud.kftg.ch>
2018-10-03 15:56:25 +0200

    Google Maps, lääääuuuft

---
+++ www/js/googlemap.js
@@ -0,0 +1,13 @@
+function myMap() {
+
+    //Map wird inizialisert
+    var mapProp= {
+        center:new google.maps.LatLng(47.368650,8.539183),
+        zoom:15,
+    };
+    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+// //
+// //
+// //
+
+}
